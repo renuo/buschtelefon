f3742fafcb0cdd46a0723fc33d7469fe97d375f1
Josua Schmid <josua.schmid@renuo.ch>
2018-12-09 14:11:51 +0100

    Add simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,14 @@
-require "bundler/setup"
-require "buschtelefon"
+require 'bundler/setup'
 require 'factory_bot'
+require 'simplecov'
+
+SimpleCov.start
+
+require 'buschtelefon'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
